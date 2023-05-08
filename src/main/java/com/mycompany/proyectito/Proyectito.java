a1c7dd2a91770aa41f765b14f512d028ee7202a4
z0s0xp <z0s0xp@gmail.com>
2023-05-07 19:15:03 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/proyectito/Proyectito.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectito;
+
+/**
+ *
+ * @author z0s0xp
+ */
+public class Proyectito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
